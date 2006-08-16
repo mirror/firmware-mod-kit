807ebd9dd5f4a7b67e622f7c0f3d143277621942
jeremy.collake <jeremy.collake@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2006-08-16 23:32:34 +0000

    Initial import. Public version v0.43 beta. - jc
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@2 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

---
+++ firmware-mod-kit/build_firmware.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+. "./shared.inc"
+VERSION='0.43 beta'
+#
+# Title: build_firmware.sh
+# Author: Jeremy Collake <jeremy.collake@gmail.com>
+# Site: http://www.bitsum.com
+#
+# Script to build a cybertan format firmware
+# with a squashfs-lzma filesystem.
+#
+# See documentation at:
+#  http://www.bitsum.com/firmware_mod_kit.htm
+#
+# USAGE: build_firmware.sh OUTPUT_DIRECTORY/ WORKING_DIRECOTRY/
+#
+# This scripts builds the firmware image from [WORKING_DIRECTORY],
+# with the following subdirectories:
+#
+#    image_parts/   <- firmware seperated
+#    rootfs/ 	    <- filesystem
+#
+# Example:
+#
+# ./build_firmware.sh new_firmwares/ std_generic/
+#
+#
+FIRMARE_BASE_NAME=custom_image
+EXIT_ON_FS_PROBLEM="0"
+
+echo "$0 v$VERSION, (c)2006 Jeremy Collake"
+
+#################################################################
+#################################################################
+
+if [ $# = 2 ]; then
+	#################################################################
+	PlatformIdentify 
+	#################################################################
+	TestFileSystemExit "$1" "$2"
+	#################################################################
+	if [ ! -e "./build_firmware.sh" ]; then
+		echo "  ERROR - You must run this script from the same directory as it is in!"
+		exit 1
+	fi
+	#################################################################
+	# remove deprecated stuff
+	if [ -e "./asustrx.c" ]; then 
+		mkdir "src_backup"
+		mv "*.c" "src_old_backup"
+		mv "*.h" "src_old_backup"
+		mv "lzma" "src_old_backup"
+		mv "Makefile" "src_old_backup"
+		Cleanup			
+	fi
+	#################################################################
+	# Invoke BuildTools, which tries to build everything and then
+	# sets up appropriate symlinks.
+	#
+	BuildTools "build.log"
+	#################################################################
+	echo "  Preparing output directory $1 ..."
+	mkdir -p $1 >> build.log 2>&1
+	rm "$1/$FIRMWARE_BASE_NAME*.*" "$1" >> build.log 2>&1
+	echo "  Building squashfs-lzma filesystem ..."
+	if [ -e "$2/image_parts/squashfs-lzma-image" ]; then	
+		# -magic to fix brainslayer changing squashfs signature in 08/10/06+ firmware images
+	 	"src/mksquashfs-lzma" "$2/rootfs/" "$2/image_parts/squashfs-lzma-image-new" -noappend -root-owned -le -magic "$2/image_parts/squashfs_magic" >> build.log
+	else
+		echo "  ERROR - Working directory contains no sqfs filesystem?"
+		exit 1
+	fi
+	echo "  Building base firmware image (generic) ..."
+	# I switched to asustrx due to bug in trx with big endian OS X. 
+	"src/asustrx" -o "$1/$FIRMARE_BASE_NAME.trx" "$2/image_parts/loader" "$2/image_parts/vmlinuz" "$2/image_parts/squashfs-lzma-image-new"  >> build.log 2>&1
+	echo "  Building base firmware image (asus) ..."	
+	"src/asustrx" -p WL500gx -v 1.9.2.7 -o "$1/$FIRMARE_BASE_NAME-asus.trx" "$2/image_parts/loader" "$2/image_parts/vmlinuz" "$2/image_parts/squashfs-lzma-image-new"  >> build.log 2>&1
+	echo "  Making $1/$FIRMARE_BASE_NAME-wrtsl54gs.bin"
+	"src/addpattern" -4 -p W54U -v v4.20.6 -i "$1/$FIRMARE_BASE_NAME.trx" -o "$1/$FIRMARE_BASE_NAME-wrtsl54gs.bin" -g >> build.log 2>&1
+	echo "  Making $1/$FIRMARE_BASE_NAME-wrt54g.bin"
+	"src/addpattern" -4 -p W54G -v v4.20.6 -i "$1/$FIRMARE_BASE_NAME.trx" -o "$1/$FIRMARE_BASE_NAME-wrt54g.bin" -g >> build.log 2>&1
+	echo "  Making $1/$FIRMARE_BASE_NAME-wrt54gs.bin"
+	"src/addpattern" -4 -p W54S -v v4.70.6 -i "$1/$FIRMARE_BASE_NAME.trx" -o "$1/$FIRMARE_BASE_NAME-wrt54gs.bin" -g >> build.log 2>&1
+	echo "  Making $1/$FIRMARE_BASE_NAME-wrt54gsv4.bin"
+	"src/addpattern" -4 -p W54s -v v1.05.0 -i "$1/$FIRMARE_BASE_NAME.trx" -o "$1/$FIRMARE_BASE_NAME-wrt54gsv4.bin" -g >> build.log 2>&1
+	echo "  Making $1/$FIRMARE_BASE_NAME-generic.bin"
+	mv "$1/$FIRMARE_BASE_NAME.trx" "$1/$FIRMARE_BASE_NAME-generic.bin" >> build.log 2>&1
+	echo "  Firmware images built."
+	ls -l "$1"
+	echo "  All done!"
+else
+	#################################################################
+	echo "  Incorrect usage."
+	echo "  USAGE: $0 OUTPUT_DIR WORKING_DIR"
+	exit 1
+fi
+exit 0
---
+++ firmware-mod-kit/extract_firmware.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+. "./shared.inc"
+VERSION='0.43 beta'
+#
+# Title: extract_firmware.sh
+# Author: Jeremy Collake <jeremy.collake@gmail.com>
+# Site: http://www.bitsum.com
+#
+# Script to extract a cybertan format firmware
+# with a squashfs-lzma filesystem.
+#
+# See documentation at:
+#  http://www.bitsum.com/firmware_mod_kit.htm
+#
+# USAGE: extract_firmware.sh FIRMWARE_IMAGE.BIN WORKING_DIRECTORY/
+#
+# This scripts extacts the firmware image to [WORKING_DIRECTORY],
+# with the following subdirectories:
+#
+#    image_parts/   <- firmware seperated
+#    rootfs/ 	    <- extracted filesystem
+#
+# Example:
+#
+# ./extract_firmware.sh dd-wrt.v23_generic.bin std_generic
+#
+#
+EXIT_ON_FS_PROBLEM="0"
+
+echo "$0 v$VERSION, (c)2006 Jeremy Collake"
+
+#################################################################
+#################################################################
+
+if [ $# = 2 ]; then
+	#################################################################
+	PlatformIdentify
+	#################################################################
+	TestFileSystemExit $1 $2
+	#################################################################
+	if [ -f "$1" ]; then
+		if [ ! -e "./extract_firmware.sh" ]; then
+			echo "  ERROR - You must run this script from the same directory as it is in!"
+			exit 1
+		fi
+		#################################################################
+		# remove deprecated stuff
+		if [ -e "./asustrx.c" ]; then 
+			mkdir "src_backup"
+			mv "*.c" "src_old_backup"
+			mv "*.h" "src_old_backup"
+			mv "lzma" "src_old_backup"
+			mv "Makefile" "src_old_backup"
+			Cleanup			
+		fi
+		#################################################################
+		# Invoke BuildTools, which tries to build everything and then
+		# sets up appropriate symlinks.
+		#
+		BuildTools "extract.log"					     
+		#################################################################		
+		echo "  Preparing working directory ..."
+		echo "   Removing any previous files ..."
+		rm -rf "$2/rootfs" >> extract.log 2>&1
+		rm -rf "$2/image_parts" >> extract.log 2>&1
+		rm -rf "$2/installed_packages" >> extract.log 2>&1
+		echo "   Creating directories ..."
+		mkdir -p "$2/image_parts" >> extract.log 2>&1
+		mkdir -p "$2/installed_packages" >> extract.log 2>&1
+		echo "  Extracting firmware ..."
+		"src/untrx" "$1" "$2/image_parts" >> extract.log
+		if [ -e "$2/image_parts/squashfs-lzma-image" ]; then	
+	 		"src/unsquashfs-lzma" -dest "$2/rootfs" "$2/image_parts/squashfs-lzma-image" >> extract.log	
+		else
+			echo "  Error extracting firmware. Check extract.log."
+			exit 1
+		fi
+		if [ -e "$2/rootfs" ]; then
+			echo "  Firmware appears extracted correctly!"
+			echo "  Now make changes and run build_firmware.sh."
+		else
+			echo "  Error: Squashfs filesystem not extracted properly."
+			echo "  Make sure the firmware image format is right."
+			exit 1
+		fi	
+	else
+		echo "  $1 does not exist.. give me something to work with man!"
+	fi
+else
+	echo "  Incorrect usage."
+	echo "  USAGE: $0 FIRMWARE_IMAGE.BIN WORKING_DIR"
+	exit 1
+fi
+exit 0
