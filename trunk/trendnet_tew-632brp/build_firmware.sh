106b186ea750989836057013a15a35b544acea26
jeremy.collake <jeremy.collake@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2008-08-23 19:29:09 +0000

    use squashfs 3.0 tools /w src
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@51 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

--- trunk/trendnet_tew-632brp/build_firmware.sh
+++ trunk/trendnet_tew-632brp/build_firmware.sh
@@ -1,10 +1,17 @@
 #/bin/sh
 echo "Building firmware for TEW-632BRP ..."
+if [ ! -e "../src/squashfs-3.0/mksquashfs-lzma" ]; then	
+	make -C "../src" 2>&1 > buildlog.bat
+	if [ ! -e "../src/squashfs-3.0/mksquashfs-lzma" ]; then
+		echo "Error building mksquashfs-lzma! Check buildlog.bat"
+		exit 1
+	fi	
+fi
 if [ ! -e "rootfs/" ]; then
 	tar -xzvf rootfs.tar.gz
 fi
 rm -f output/squashfs-3-lzma.img output/newfirmware.rom
-./mksquashfs_ap71 ./rootfs/ output/squashfs-3-lzma.img -all-root -be -noappend
+../src/squashfs-3.0/mksquashfs-lzma ./rootfs/ output/squashfs-3-lzma.img -all-root -be -noappend
 cp vmlinuz output/newfirmware.rom
 dd if=output/squashfs-3-lzma.img of=output/newfirmware.rom bs=1K seek=1024
 cat ./hwid.txt >> output/newfirmware.rom
