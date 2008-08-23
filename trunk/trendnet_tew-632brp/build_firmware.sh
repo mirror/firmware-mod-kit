461fb8714a762df8f6054a882aa586234287ff06
jeremy.collake <jeremy.collake@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2008-08-23 21:21:08 +0000

    reorganization
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@53 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

--- trunk/trendnet_tew-632brp/build_firmware.sh
+++ trunk/trendnet_tew-632brp/build_firmware.sh
@@ -1,17 +1,30 @@
 #/bin/sh
+#
+# Script written by Jeremy Collake (jeremy.collake@gmail.com)
+# Bitsum Technologies http://www.bitsum.com
+#
+PARTS_PATH="./image_parts"
+OUTPUT_PATH="./firmware_image_output"
+OUTPUT_FIRMWARE_FILENAME="tew-632brp-fmk-firmware.bin"
 echo "Building firmware for TEW-632BRP ..."
+echo "NOTE: This script should be run as root!"
 if [ ! -e "../src/squashfs-3.0/mksquashfs-lzma" ]; then	
-	make -C "../src" 2>&1 > buildlog.bat
+	make -C "../src" 2>&1 > buildlog.log
 	if [ ! -e "../src/squashfs-3.0/mksquashfs-lzma" ]; then
-		echo "Error building mksquashfs-lzma! Check buildlog.bat"
+		echo "Error building mksquashfs-lzma! Check buildlog.log"
 		exit 1
 	fi	
 fi
+#
+# unsquashfs the root filesystem
+#
 if [ ! -e "rootfs/" ]; then
-	tar -xzvf rootfs.tar.gz
+	"../src/squashfs-3.0/unsquashfs-lzma" "image_parts/squashfs" -dest "./rootfs"
+	#tar -xzvf rootfs.tar.gz
 fi
-rm -f output/squashfs-3-lzma.img output/newfirmware.rom
-../src/squashfs-3.0/mksquashfs-lzma ./rootfs/ output/squashfs-3-lzma.img -all-root -be -noappend
-cp vmlinuz output/newfirmware.rom
-dd if=output/squashfs-3-lzma.img of=output/newfirmware.rom bs=1K seek=1024
-cat ./hwid.txt >> output/newfirmware.rom
+mkdir -p "$OUTPUT_PATH"
+rm -f $PARTS_PATH/squashfs-3-lzma.img "$OUTPUT_PATH/$OUTPUT_FIRMWARE_FILENAME"
+../src/squashfs-3.0/mksquashfs-lzma ./rootfs/ $PARTS_PATH/squashfs-3-lzma.img -all-root -be -noappend
+cp $PARTS_PATH/vmlinuz $OUTPUT_PATH/$OUTPUT_FIRMWARE_FILENAME
+dd if=$PARTS_PATH/squashfs-3-lzma.img "of=$OUTPUT_PATH/$OUTPUT_FIRMWARE_FILENAME" bs=1K seek=1024
+cat $PARTS_PATH/hwid.txt >> "$OUTPUT_PATH/$OUTPUT_FIRMWARE_FILENAME"
