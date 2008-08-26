c6439639a9e94f77460f4a119f0a3826efc9d2d0
jeremy.collake <jeremy.collake@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2008-08-26 14:02:31 +0000

    remove forced exit on any error, only force exit on block read error
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@81 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

--- trunk/src/squashfs-3.0/unsquashfs.c
+++ trunk/src/squashfs-3.0/unsquashfs.c
@@ -64,7 +64,7 @@
 
 #define EXIT_UNSQUASH(s, args...)	do { \
 						fprintf(stderr, "FATAL ERROR aborting: "s, ## args); \
-					} while(0); exit(1) 
+					} while(0)
 					
 
 struct hash_table_entry {
@@ -239,6 +239,7 @@ void uncompress_inode_table(long long start, long long end, squashfs_super_block
 		if((res = read_block(start, &start, inode_table + bytes, sBlk)) == 0) {
 			free(inode_table);
 			EXIT_UNSQUASH("uncompress_inode_table: failed to read block\n");
+			exit(1);
 		}
 		bytes += res;
 	}
