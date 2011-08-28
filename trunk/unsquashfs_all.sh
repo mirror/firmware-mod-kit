caff412935cb127d39731f0eb9b6c0dc77d96ef4
heffnercj <heffnercj@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2011-08-28 01:05:45 +0000

    Added unsquashfs_all.sh script. Uncommented squashfs-3.2-r2-lzma build in src/others/Makefile.
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@188 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

---
+++ trunk/unsquashfs_all.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Script to attempt to extract files from a SquashFS image using all of the available unsquashfs utilities in FMK until one is successful.
+#
+# Craig Heffner
+# 27 August 2011
+
+IMG="$1"
+DIR="$2"
+
+ROOT="./src"
+SUBDIRS="squashfs-2.1-r2 squashfs-3.0 squashfs-3.0-lzma-damn-small-variant others/squashfs-3.2-r2 others/squashfs-3.2-r2-lzma others/squashfs-3.2-r2-lzma/squashfs3.2-r2/squashfs-tools others/squashfs-3.3 others/squashfs-3.3-lzma/squashfs3.3/squashfs-tools"
+EXE=""
+DEST=""
+
+if [ "$IMG" == "" ] || [ "$IMG" == "-h" ]
+then
+	echo "Usage: $0 <squashfs image> [output directory]"
+	exit 1
+fi
+
+if [ "$DIR" != "" ]
+then
+	DEST="-dest $DIR"
+fi
+
+for SUBDIR in $SUBDIRS
+do
+	unsquashfs="$ROOT/$SUBDIR/unsquashfs"
+
+	if [ -e $unsquashfs ]
+	then
+		echo -n "$ROOT/$SUBDIR/unsquashfs: "
+		$unsquashfs $DEST $IMG
+		
+		if [ $? == 0 ]
+		then
+			EXE=$unsquashfs
+		fi
+	fi
+	
+	if [ "$EXE" == "" ] && [ -e $unsquashfs-lzma ]
+	then
+		echo -n "$unsquashfs-lzma: "
+		$unsquashfs-lzma $DEST $IMG
+        
+                if [ $? == 0 ]
+                then
+                        EXE="$unsquashfs-lzma"
+                fi
+	fi
+
+	if [ "$EXE" != "" ]
+	then
+		echo "File system sucessfully extracted ($EXE)"
+		exit 0
+	fi
+done
+
+echo "File extraction failed!"
+exit 1
