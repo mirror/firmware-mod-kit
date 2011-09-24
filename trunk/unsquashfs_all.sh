1f838835db184dcd3c9a540400aefc631dfa2391
heffnercj <heffnercj@5c1471b0-2a1b-0410-b4f4-f33460ac40b9>
2011-09-24 00:09:38 +0000

    Added support for Linksys E series squashfs file systems
    git-svn-id: http://firmware-mod-kit.googlecode.com/svn/trunk@245 5c1471b0-2a1b-0410-b4f4-f33460ac40b9

--- trunk/unsquashfs_all.sh
+++ trunk/unsquashfs_all.sh
@@ -8,7 +8,7 @@ IMG="$1"
 DIR="$2"
 
 ROOT="./src"
-SUBDIRS="squashfs-2.1-r2 squashfs-3.0 squashfs-3.0-lzma-damn-small-variant others/squashfs-3.2-r2 others/squashfs-3.2-r2-lzma others/squashfs-3.2-r2-lzma/squashfs3.2-r2/squashfs-tools others/squashfs-3.3 others/squashfs-3.3-lzma/squashfs3.3/squashfs-tools"
+SUBDIRS="squashfs-2.1-r2 squashfs-3.0 squashfs-3.0-lzma-damn-small-variant others/squashfs-3.0-e2100 others/squashfs-3.2-r2 others/squashfs-3.2-r2-lzma others/squashfs-3.2-r2-lzma/squashfs3.2-r2/squashfs-tools others/squashfs-3.3 others/squashfs-3.3-lzma/squashfs3.3/squashfs-tools"
 TIMEOUT="5"
 MKFS=""
 DEST=""
